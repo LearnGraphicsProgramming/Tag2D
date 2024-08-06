f37446900d839eed9b8fd158457dda19a0018e7d
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2024-08-06 21:06:40 +0300

    Server socket can accept TCP connections on Windows

--- server/src/Socket.cpp
+++ server/src/Socket.cpp
@@ -1,12 +1,97 @@
 #include "Socket.h"
+#include "Logger.h"
 
 namespace Tag2D
 {
 	Socket::Socket()
+		: m_Address{ 0 }, m_Port(-1), m_ListenSocket(INVALID_SOCKET), m_SocketAddrStruct(sockaddr_in())
+#ifdef WINDOWS
+		, m_WSAData(WSADATA())
+#endif
 	{
+#ifdef DEBUG
+		log_info("Creating server object...");
+#endif
 	}
 
 	Socket::~Socket()
 	{
+#ifdef WINDOWS
+		WSACleanup();
+		closesocket(m_ListenSocket);
+#else
+		close(m_ListenSocket);
+#endif
+	}
+
+	bool Socket::Init(const char* address, uint16_t port)
+	{
+		int result = SOCKET_ERROR;
+		log_info("Initializing socket..");
+
+#ifdef WINDOWS
+		result = WSAStartup(MAKEWORD(2, 2), &m_WSAData);
+
+		if (result != 0)
+		{
+			log_critical("Failed to start Windows WSA");
+			return false;
+		}
+#endif
+
+		m_ListenSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+		if (m_ListenSocket == SOCKET_ERROR)
+		{
+			log_critical("Failed to create socket (no socket descriptor returned)");
+			return false;
+		}
+
+		strncpy_s(m_Address, sizeof(m_Address), address, MAX_IP_ADDRESS_LENGTH - 1);
+		m_Port = port;
+		
+		memset(&m_SocketAddrStruct, 0, sizeof(m_SocketAddrStruct));
+		m_SocketAddrStruct.sin_family = AF_INET;
+		m_SocketAddrStruct.sin_port = htons(m_Port);
+		inet_pton(AF_INET, m_Address, &m_SocketAddrStruct.sin_addr);
+
+		if (bind(m_ListenSocket, (sockaddr*)&m_SocketAddrStruct, sizeof(m_SocketAddrStruct)) == SOCKET_ERROR)
+		{
+			log_critical("Failed to bind socket");
+			return false;
+		}
+
+		if (listen(m_ListenSocket, SOMAXCONN) == SOCKET_ERROR)
+		{
+			log_critical("Failed to listen");
+			return false;
+		}
+
+		log_info("Server listening at ");
+		std::cout << address << ":" << port << "\n";
+		
+		return true;
+	}
+
+	void Socket::OnFrame() const
+	{
+		sockaddr_in clientSocketAddr{};
+		int clientSocketAddrSize = sizeof(clientSocketAddr);
+
+		SOCKET clientSocket = accept(m_ListenSocket, (sockaddr*)&clientSocket, &clientSocketAddrSize);
+
+		if (clientSocket != INVALID_SOCKET)
+		{
+			log_info("New connection to the server!");
+			closesocket(clientSocket);
+		}
+		else
+		{
+#ifdef WINDOWS
+			log_critical(std::to_string(WSAGetLastError()));
+#else
+			perror("accept");
+#endif
+		}
 	}
 }
--- server/src/Socket.h
+++ server/src/Socket.h
@@ -1,14 +1,43 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
+#include "pc.h"
+
+#include <cstdint>
+#include <string>
+
+#ifdef WINDOWS
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+#endif
+
+#ifndef INVALID_SOCKET
+#define INVALID_SOCKET -1
+#endif
 
 namespace Tag2D
 {
 	// Multi-platform Socket object
 	class Socket
 	{
+	public:
 		Socket();
-		~Socket();
+		virtual ~Socket();
+
+		bool Init(const char* address, uint16_t port);
+		void OnFrame() const;
+	
+	private:
+		char m_Address[MAX_IP_ADDRESS_LENGTH];
+		uint16_t m_Port;
+
+#ifdef WINDOWS
+		WSADATA m_WSAData;
+#endif
+		sockaddr_in m_SocketAddrStruct;
+
+		uint64_t m_ListenSocket;
 	};
 }
 #endif // SOCKET_H
