91d0275acf22bf32297dd17e244862c825f7b907
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-08-06 13:21:20 -0400

    Add submodule update command to build generation scripts.

--- scripts/GenerateMacOSMake.sh
+++ scripts/GenerateMacOSMake.sh
@@ -1,3 +1,4 @@
 cd ..
+git submodule update
 ./scripts/premake-5.0.0-beta2-macos/premake5 gmake $1
 read -p "Press enter to continue"
--- scripts/GenerateWindowsMake.bat
+++ scripts/GenerateWindowsMake.bat
@@ -1,5 +1,6 @@
 @echo off
 pushd %~dp0\..\
+git submodule update
 call scripts\premake-5.0.0-beta2-windows\premake5.exe gmake %*
 popd
 PAUSE
