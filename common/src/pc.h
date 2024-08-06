0ebc9a361e1e1651653c905f537bfb738515ab58
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2024-08-06 19:18:14 +0300

    Update pc.h
    Added macros for cross platform identify.

--- common/src/pc.h
+++ common/src/pc.h
@@ -3,6 +3,28 @@
 
 // Common precompiled headers
 
+#define DEBUG
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#ifdef _WIN64
+#define WINDOWS
+#else // _WINX86
+#error Platform Windows x86 not supported
+#endif // WINDOWS
+
+#elif __APPLE__
+#define APPLE
+
+#elif __linux__
+#define LINUX
+
+#elif __APPLE__
+#define APPLE
+
+#endif // __APPLE__
+
+#define ASSERT(bool_value, text) ( bool_value && std::cout << "[Error Occured] ASSERTION: " << text  << "\n")
+
 #define LOCAL_ADDRESS "127.0.0.1"
 
 #define SERVER_ADDRESS LOCAL_ADDRESS
