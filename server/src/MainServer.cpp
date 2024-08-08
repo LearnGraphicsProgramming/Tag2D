998fed79ee8692ea421b6cf3e73326f4c18b6366
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2024-08-08 17:26:58 +0300

    Update MainServer.cpp

--- server/src/MainServer.cpp
+++ server/src/MainServer.cpp
@@ -14,16 +14,14 @@ int main()
 {
 	InitGlobals();
 
-	std::shared_ptr<Tag2D::Console> console = std::make_shared<Tag2D::Console>();
-
 	std::shared_ptr<Tag2D::Server> server = std::make_shared<Tag2D::Server>();
 	server->Init(SERVER_ADDRESS, SERVER_PORT);
 
-	server->RegisterOnFrameCallback([console]() { console->OnFrame(); });
-	
+	std::shared_ptr<Tag2D::Console> console = std::make_shared<Tag2D::Console>();
 	std::shared_ptr<FrameCounter> frameCounter = std::make_shared<FrameCounter>();
-	server->RegisterOnFrameCallback([frameCounter]() { frameCounter->OnFrame(); });
 
+	server->RegisterOnFrameCallback([console]() { console->OnFrame(); });
+	server->RegisterOnFrameCallback([frameCounter]() { frameCounter->OnFrame(); });
 
 	server->Start();
 
