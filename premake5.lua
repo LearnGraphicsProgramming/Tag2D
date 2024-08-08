318fbc5fcbacedd14bb8266b57d735cbdd238795
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-08-08 12:35:30 -0400

    Change premake file to only change architecture to ARM64 on macosx, and leave it as x64 on other systems.

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,7 @@
 workspace "Tag2D"
+	architecture "x64"
 
-	filter "system:windows"
-		architecture "x64"
-	filter "system:macosx"
+	filter "system:macosx" -- If the architecture is MacOSX, then we want to set the architecture to ARM64
 		architecture "ARM64"
 
 	configurations { "Debug", "Release", "Dist" } -- Debug, Release, Distribution. Debug has symbols, Release has optimizations, Dist has no symbols and optimizations.
